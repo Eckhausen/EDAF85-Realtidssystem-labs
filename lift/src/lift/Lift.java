280ea8aeee23c8a296654ef93afaa478d576ffed
Eknz350 <80269043+Eknz350@users.noreply.github.com>
2023-09-15 13:46:21 +0200

    Push

--- lift/src/lift/Lift.java
+++ lift/src/lift/Lift.java
@@ -4,5 +4,10 @@ public class Lift {
     private int currentFloor;
     private int nextFloor;
     private LiftMonitor monitor;
+    private LiftView liftView;
+    public Lift(LiftView view, LiftMonitor monitor){
+        this.liftView = view;
+        this.monitor = monitor;
+    }
 
 }
--- lift/src/lift/Pax.java
+++ lift/src/lift/Pax.java
@@ -3,14 +3,16 @@ package lift;
 public class Pax extends Thread{
     private int startFloor;
     private int destinationFloor;
-    private Passenger passengerView;
+    private Passenger passenger;
+    private LiftMonitor monitor;
 
     public Pax(Passenger passenger, LiftMonitor monitor){
         this.startFloor = passenger.getStartFloor();
         this.destinationFloor = passenger.getDestinationFloor();
-        this.passengerView = passenger;
+        this.passenger = passenger;
+        this.monitor = monitor;
     }
     public void run(){
-
+        passenger.begin();
     }
 }
