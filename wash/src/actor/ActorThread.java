a6b81bb04bd5209a1b09bf4f2b80c82cb2efd2b0
Eckhausen <anton.eckhausen98@gmail.com>
2023-10-09 21:08:44 +0200

    ActorThread complete, no errors

--- wash/src/actor/ActorThread.java
+++ wash/src/actor/ActorThread.java
@@ -1,23 +1,28 @@
 package actor;
 
-public class ActorThread<M> extends Thread {
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
 
+public class ActorThread<M> extends Thread {
+    private LinkedBlockingQueue<M> queue = new LinkedBlockingQueue<>();
     /** Called by another thread, to send a message to this thread. */
     public void send(M message) {
-        // TODO: implement this method (one or a few lines)
+        try{
+            queue.put(message);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
     }
     
     /** Returns the first message in the queue, or blocks if none available. */
     protected M receive() throws InterruptedException {
-        // TODO: implement this method (one or a few lines)
-        return null;
+        return queue.take();
     }
     
     /** Returns the first message in the queue, or blocks up to 'timeout'
         milliseconds if none available. Returns null if no message is obtained
         within 'timeout' milliseconds. */
     protected M receiveWithTimeout(long timeout) throws InterruptedException {
-        // TODO: implement this method (one or a few lines)
-        return null;
+        return queue.poll(timeout, TimeUnit.MILLISECONDS);
     }
 }
