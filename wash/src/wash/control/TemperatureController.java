aedd94a25d8761904591bbeb4532b23a17548fa6
Eckhausen <anton.eckhausen98@gmail.com>
2023-10-11 17:15:00 +0200

    Temp controller WIP

--- wash/src/wash/control/TemperatureController.java
+++ wash/src/wash/control/TemperatureController.java
@@ -30,45 +30,48 @@ public class TemperatureController extends ActorThread<WashingMessage> {
     public void run() {
         // TODO
         try {
-            WashingMessage m = receiveWithTimeout(10000 / Settings.SPEEDUP);
-            if (m != null) {
-                System.out.println("got " + m);
-            }
+            while(true){
+                WashingMessage m = receiveWithTimeout(10000 / Settings.SPEEDUP);
+                if (m != null) {
+                    System.out.println("got " + m);
 
-            switch (m.order()) {
-                case TEMP_SET_40:
-                    selectedTemp = 40;
-                    break;
-                case TEMP_SET_60:
-                    selectedTemp = 60;
-                    break;
-                case TEMP_IDLE:
+                    switch (m.order()) {
+                        case TEMP_SET_40:
+                            selectedTemp = 40;
+                            break;
+                        case TEMP_SET_60:
+                            selectedTemp = 60;
+                            break;
+                        case TEMP_IDLE:
+                            selectedTemp = 0;
+                            break;
+                    }
+                }
+
+                currentTemp = io.getTemperature();
+
+                if (selectedTemp == 40) {
+                    if (currentTemp < 38.2 && !heaterOn) {
+                        io.heat(true);
+                        heaterOn = true;
+                    } else if (currentTemp >= 39.8 && heaterOn) {
+                        io.heat(false);
+                        heaterOn = false;
+                    }
+                } else if (selectedTemp == 60) {
+                    if (currentTemp < 58.2 && !heaterOn) {
+                        io.heat(true);
+                        heaterOn = true;
+                    } else if (currentTemp >= 59.8 && heaterOn) {
+                        io.heat(false);
+                        heaterOn = false;
+                    }
+                } else if (selectedTemp == 0){
                     io.heat(false);
                     heaterOn = false;
-                    selectedTemp = 0;
                     m.sender().send(new WashingMessage(this, WashingMessage.Order.ACKNOWLEDGMENT));
-                    break;
-            }
-            currentTemp = io.getTemperature();
-
-            if (selectedTemp == 40) {
-                if (currentTemp < 38.2 && !heaterOn) {
-                    io.heat(true);
-                    heaterOn = true;
-                } else if (currentTemp >= 39.8 && heaterOn) {
-                    io.heat(false);
-                    heaterOn = false;
-                }
-            } else if (selectedTemp == 60) {
-                if (currentTemp < 58.2 && !heaterOn) {
-                    io.heat(true);
-                    heaterOn = true;
-                } else if (currentTemp >= 59.8 && heaterOn) {
-                    io.heat(false);
-                    heaterOn = false;
                 }
             }
-
 
         } catch (InterruptedException unexpected) {
             throw new RuntimeException(unexpected);
